62eeb7bfd5094629172c4cff683446ce3afe27ac
Abbe Keultjes <flauwekeul@gmail.com>
2019-06-01 11:48:19 +0200

    implement own neighborsOf() method until it's fixed in honeycomb

--- src/world/index.js
+++ src/world/index.js
@@ -5,6 +5,19 @@ import { Grid } from '../grid';
 import { MAX_PHEROMONE, TILE_TYPES } from '../settings';
 import './world.css';
 
+const DIRECTION_COORDINATES = [
+  { q: 1, r: 0 },
+  { q: 0, r: 1 },
+  { q: -1, r: 1 },
+  { q: -1, r: 0 },
+  { q: 0, r: -1 },
+  { q: 1, r: -1 }
+]
+
+function signedModulo(dividend, divisor) {
+  return ((dividend % divisor) + divisor) % divisor
+}
+
 export class World {
   get tiles() { return this.grid.hexes }
 
@@ -34,7 +47,15 @@ export class World {
   addAnt({ direction = 0 } = {}) {
     const ant = new Ant({
       draw: this.draw,
-      surroundingTiles: (tile, direction) => this.tiles.neighborsOf(tile, direction),
+      // todo: honeycomb: grid.neighborsOf() shouldn't filter out empty hexes and ideally should map each direction to a hex
+      // surroundingTiles: (tile, direction) => this.tiles.neighborsOf(tile, direction),
+      surroundingTiles: (tile, directions = [0, 1, 2, 3, 4, 5]) => [].concat(directions).map(direction => {
+        if (direction < 0 || direction > 5) {
+          direction = signedModulo(direction, 6)
+        }
+        const { q, r } = DIRECTION_COORDINATES[direction]
+        return this.tiles.get(tile.cubeToCartesian({ q: tile.q + q, r: tile.r + r }))
+      }),
       // returns first tile the ant should go to in order to return to the nest
       tileTowardsNest: tile => this.tiles.hexesBetween(tile, this.nestTile)[1],
       tile: this.nestTile,
